95da32f173036a47ebd93fbe048bae5cd3ccbbd1
root <root@DESKTOP-PME2N3E>
2024-01-17 19:16:57 +0800

    apply camera rotate & zoom

--- Assets/ThirdPersonCamera.cs
+++ Assets/ThirdPersonCamera.cs
@@ -1,18 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.InputSystem;
 
 public class ThirdPersonCamera : MonoBehaviour
 {
     public Transform Target;
-    public float MouseSensitivity = 10f;
+    public float RotateSpeed = 20f;
+    public float ZoomSpeed = 10f;
+    public float FarthestDistance = 30f;
+    public float NearestDistance = 3f;
 
     private Transform _cameraPivot;
 
     private float verticalRotation;
     private float horizontalRotation;
 
+    [SerializeField]
+    private InputAction press, touch0Pos, touch0Delta, touch1Contact, touch1Pos, touch1Delta;
+    private Coroutine rotateCoroutine, zoomCoroutine;
+
     private void Awake()
     {
         _cameraPivot = gameObject.transform.parent;
+    }
+
+    private void OnEnable()
+    {
+        press.Enable();
+        touch0Pos.Enable();
+        touch1Pos.Enable();
+        touch1Contact.Enable();
+        touch0Delta.Enable();
+        touch1Delta.Enable();
+    }
+    private void OnDisable()
+    {
+        press.Disable();
+        touch0Pos.Disable();
+        touch1Pos.Disable();
+        touch1Contact.Disable();
+        touch0Delta.Disable();
+        touch1Delta.Disable();
+    }
+
+    private void Start()
+    {
+        // Rotate?
+        press.performed += _ =>
+        {
+            // Only rotate when pressed at right side of the screen.
+            if (touch0Pos.ReadValue<Vector2>().x > Screen.width * 0.4) StartRotate();
+        };
+        press.canceled += _ => { StopRotate(); };
+        // Zoom?
+        touch1Contact.performed += _ =>
+        {
+            // Only zoom when first pressed is available.
+            if (touch0Pos.ReadValue<Vector2>().x > Screen.width * 0.4) StartZoom();
+        };
+        touch1Contact.canceled += _ => { StopZoom(); };
     }
 
     void LateUpdate()
@@ -22,16 +70,86 @@ public class ThirdPersonCamera : MonoBehaviour
             return;
         }
 
+        // Follow Player
         _cameraPivot.position = Target.position;
+    }
 
-        float mouseX = Input.GetAxis("Mouse X");
-        float mouseY = Input.GetAxis("Mouse Y");
+    private void StartRotate()
+    {
+        rotateCoroutine = StartCoroutine(RotateDetection());
+    }
+    private void StopRotate()
+    {
+        if (rotateCoroutine != null)
+        StopCoroutine(rotateCoroutine);
+    }
 
-        verticalRotation -= mouseY * MouseSensitivity;
-        verticalRotation = Mathf.Clamp(verticalRotation, -70f, 70f);
+    IEnumerator RotateDetection()
+    {
+        while (true)
+        {
+            Vector2 touchDelta = touch0Delta.ReadValue<Vector2>();
+            if (touch1Contact.IsPressed())
+            {
+                touchDelta = (touchDelta + touch1Delta.ReadValue<Vector2>()) / 2;
+            }
+            
+            // Calculate New Rotation
+            verticalRotation -= touchDelta.y * Time.deltaTime * RotateSpeed;
+            verticalRotation = Mathf.Clamp(verticalRotation, -70f, 70f);
 
-        horizontalRotation += mouseX * MouseSensitivity;
+            horizontalRotation += touchDelta.x * Time.deltaTime * RotateSpeed;
 
-        _cameraPivot.rotation = Quaternion.Euler(verticalRotation, horizontalRotation, 0);
+            _cameraPivot.rotation = Quaternion.Euler(verticalRotation, horizontalRotation, 0);
+            yield return null;
+        }
+    }
+    
+    private float PinchDist()
+    {
+        Vector2 pos0 = touch0Pos.ReadValue<Vector2>();
+        Vector2 pos1 = touch1Pos.ReadValue<Vector2>();
+        return (pos0 - pos1).magnitude;
+    }
+    private void StartZoom()
+    {
+        zoomCoroutine = StartCoroutine(ZoomDetection());
+    }
+    private void StopZoom()
+    {
+        if (zoomCoroutine != null)
+        StopCoroutine (zoomCoroutine);
+    }
+    IEnumerator ZoomDetection()
+    {
+        float prePinchDist = PinchDist();
+        while (true)
+        {
+            float curPinchDist = PinchDist();
+            float distDelta = curPinchDist - prePinchDist;
+            
+            // New Camera Position
+            Vector3 newCamPos = transform.localPosition;
+            newCamPos -= distDelta * Time.deltaTime * ZoomSpeed * newCamPos.normalized;
+
+            // Clamp Magnitude
+            float dist = newCamPos.magnitude;
+            if (newCamPos.z > 0) // Disable Negative
+            {
+                newCamPos = -newCamPos.normalized * NearestDistance;
+            }
+            if (dist > FarthestDistance)
+            {
+                newCamPos = newCamPos.normalized * FarthestDistance;
+            }
+            else if (dist < NearestDistance)
+            {
+                newCamPos = newCamPos.normalized * NearestDistance;
+            }
+
+            transform.localPosition = newCamPos;
+            prePinchDist = curPinchDist;
+            yield return null;
+        }
     }
 }
