6da3787b1c2f7d3848033d8c09b77420c5df9bac
cccbeginner <arvin.tsai.0113@gmail.com>
2024-01-27 18:19:07 +0800

    make my own stick

---
+++ Assets/Scripts/TouchInput/MyScreenStick.cs
@@ -0,0 +1,124 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem.Layouts;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.OnScreen;
+using UnityEngine.Serialization;
+using UnityEngine.InputSystem.Utilities;
+using UnityEngine.UI;
+using ExitGames.Client.Photon;
+using Touch = UnityEngine.InputSystem.EnhancedTouch.Touch;
+using UnityEngine.Events;
+
+public class MyScreenStick : OnScreenControl
+{
+    public float StickRaduis = 50f;
+
+    [SerializeField]
+    private GameObject JoyStickFront;
+    [SerializeField]
+    private GameObject JoyStickBack;
+
+    private Vector2 m_StartPos;
+    private Vector2 m_CurrentPos;
+    private Vector2 m_StickFrontInitPos;
+    private Vector2 m_StickBackInitPos;
+    private RectTransform m_StickFrontTransform;
+    private RectTransform m_StickBackTransform;
+
+    private StickState m_State;
+
+    private enum StickState
+    {
+        Start, Moving, End, Stay
+    }
+    public void OnStickTouch(Touch[] touches)
+    {
+        if (touches.Length == 0)
+        {
+            if (m_State == StickState.Start || m_State == StickState.Moving)
+            {
+                m_State = StickState.End;
+            }
+            else
+            {
+                m_State = StickState.Stay;
+            }
+        }
+        else
+        {
+            if (m_State == StickState.End || m_State == StickState.Stay)
+            {
+                m_State = StickState.Start;
+            }
+            else
+            {
+                m_State = StickState.Moving;
+            }
+        }
+
+        switch (m_State)
+        {
+            case StickState.Start:
+                m_StartPos = touches[0].screenPosition;
+                StartControl();
+                break;
+            case StickState.Moving:
+                m_CurrentPos = touches[0].screenPosition;
+                UpdateControl();
+                break;
+            case StickState.End:
+                EndControl();
+                break;
+        }
+    }
+
+    void Start()
+    {
+        var touchManager = GetComponent<TouchManager>();
+        var action = new UnityAction<Touch[]>(OnStickTouch);
+        touchManager.OnTouchScreenLeft.AddListener(action);
+
+        m_StickFrontTransform = JoyStickFront.GetComponent<RectTransform>();
+        m_StickFrontInitPos = m_StickFrontTransform.anchoredPosition;
+        m_StickBackTransform = JoyStickBack.GetComponent<RectTransform>();
+        m_StickBackInitPos = m_StickBackTransform.anchoredPosition;
+        SendValueToControl(Vector2.zero);
+
+        m_State = StickState.Stay;
+    }
+
+    void StartControl()
+    {
+        m_StickFrontTransform.anchoredPosition = m_StartPos;
+        m_StickBackTransform.anchoredPosition = m_StartPos;
+    }
+    void UpdateControl()
+    {
+        Vector2 resultVec = (m_CurrentPos - m_StartPos) / StickRaduis;
+        if (resultVec.sqrMagnitude > 1)
+        {
+            resultVec = resultVec.normalized;
+        }
+        SendValueToControl(resultVec);
+        m_StickFrontTransform.anchoredPosition = m_StartPos + resultVec * StickRaduis;
+    }
+    void EndControl()
+    {
+        m_StickFrontTransform.anchoredPosition = m_StickFrontInitPos;
+        m_StickBackTransform.anchoredPosition = m_StickBackInitPos;
+        SendValueToControl(Vector2.zero);
+    }
+
+    [InputControl(layout = "Vector2")]
+    [SerializeField]
+    private string m_ControlPath;
+    protected override string controlPathInternal
+    {
+        get => m_ControlPath;
+        set => m_ControlPath = value;
+    }
+}
---
+++ Assets/Scripts/TouchInput/TouchManager.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem.EnhancedTouch;
+using Touch = UnityEngine.InputSystem.EnhancedTouch.Touch;
+
+public class TouchManager : MonoBehaviour
+{
+    public float ScreenMiddle = 0.5f;
+
+    public UnityEvent<Touch[]> OnTouchScreenLeft;
+    public UnityEvent<Touch[]> OnTouchScreenRight;
+
+    // Maintain currently valid touches.
+    // All valid touches must on right side of the screen.
+    // All valid touches must not be a tap.
+    List<Touch> m_LeftTouches = new List<Touch>();
+    List<Touch> m_RightTouches = new List<Touch>();
+
+    protected void OnEnable()
+    {
+        EnhancedTouchSupport.Enable();
+    }
+
+    protected void OnDisable()
+    {
+        EnhancedTouchSupport.Disable();
+    }
+
+    private void RemoveTouch(Touch touch)
+    {
+        m_LeftTouches.RemoveAll(_touch => _touch.touchId == touch.touchId);
+        m_RightTouches.RemoveAll(_touch => _touch.touchId == touch.touchId);
+    }
+
+    private void AddTouch(Touch touch)
+    {
+        if (touch.screenPosition.x > Screen.width * ScreenMiddle)
+        {
+            m_RightTouches.Add(touch);
+        }
+        else
+        {
+            m_LeftTouches.Add(touch);
+        }
+    }
+
+    private void UpdateTouch(Touch touch)
+    {
+        int idx = m_RightTouches.FindIndex(0, m_RightTouches.Count, _touch => _touch.touchId == touch.touchId);
+        if (0 <= idx && idx < m_RightTouches.Count)
+        {
+            m_RightTouches[idx] = touch;
+        }
+        idx = m_LeftTouches.FindIndex(0, m_LeftTouches.Count, _touch => _touch.touchId == touch.touchId);
+        if (0 <= idx && idx < m_LeftTouches.Count)
+        {
+            m_LeftTouches[idx] = touch;
+        }
+    }
+
+    private void Update()
+    {
+        // Update Current Touches that Rotates/Zooms Camera.
+        for (int i = 0; i < Touch.activeTouches.Count; i++)
+        {
+            Touch touch = Touch.activeTouches[i];
+            if (touch.began)
+            {
+                // Prevent from quickly switching fingers.
+                RemoveTouch(touch);
+                AddTouch(touch);
+            }
+            else if (touch.ended)
+            {
+                RemoveTouch(touch);
+            }
+            else
+            {
+                UpdateTouch(touch);
+            }
+        }
+        OnTouchScreenLeft.Invoke(m_LeftTouches.ToArray());
+        OnTouchScreenRight.Invoke(m_RightTouches.ToArray());
+    }
+}
