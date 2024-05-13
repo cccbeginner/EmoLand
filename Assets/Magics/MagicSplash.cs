b76d364e579492a3009d56ce350ccac6b2f72cea
cccbeginner <arvin.tsai.0113@gmail.com>
2024-05-13 09:40:00 +0800

    add player splash effect

--- Assets/Magics/MagicSplash.cs
+++ Assets/Magics/MagicSplash.cs
@@ -3,66 +3,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MagicSplash : NetworkBehaviour
+public class MagicSplash : MonoBehaviour
 {
-    private ParticleSystem m_ParticalSystem;
-    private Player m_MainPlayer;
-    private CharacterController m_PlayerController;
-    private Vector3 m_PlayerPosPrev;
+    [SerializeField] Player player;
+    [SerializeField] GameObject SplashParticle;
+    Vector3 m_InitScale;
 
-    [Networked]
-    bool nt_EnableSplash {  get; set; }
-
-    public override void Spawned()
+    public void Start()
     {
-        if (!HasStateAuthority) return;
-        m_ParticalSystem = transform.GetChild(0).gameObject.GetComponent<ParticleSystem>();
-        m_ParticalSystem.Stop();
+        m_InitScale = SplashParticle.transform.localScale;
+        player.droplet.OnResize.AddListener(SetParticleSize);
+        SetParticleSize(player.droplet.size);
     }
 
-    public override void FixedUpdateNetwork()
+    private void SetParticleSize(int size)
     {
-        if (!HasStateAuthority) return;
-        if (m_MainPlayer == null)
-        {
-            m_MainPlayer = Player.main;
-            if (m_MainPlayer == null)
-            {
-                return;
-            }
-            m_PlayerController = m_MainPlayer.GetComponent<CharacterController>();
-            m_PlayerPosPrev = m_MainPlayer.transform.position;
-        }
-        Vector3 currentPos = m_MainPlayer.transform.position;
-
-        // Follow main player.
-        transform.position = currentPos;
-        transform.forward = m_MainPlayer.transform.forward;
-
-        // Enable splash if player is moving on ground, disable vice versa.
-        if (m_PlayerController.isGrounded && (currentPos - m_PlayerPosPrev).magnitude >= Time.deltaTime)
-        {
-            nt_EnableSplash = true;
-        }
-        else
-        {
-            nt_EnableSplash = false;
-        }
-        m_PlayerPosPrev = currentPos;
-    }
-    public override void Render()
-    {
-        if (m_MainPlayer == null)
-        {
-            return;
-        }
-        if (nt_EnableSplash == true && m_ParticalSystem.isPlaying == false)
-        {
-            m_ParticalSystem.Play();
-        }
-        else if (nt_EnableSplash == false && m_ParticalSystem.isPlaying == true)
-        {
-            m_ParticalSystem.Stop();
-        }
+        float scale = Mathf.Pow(size, 1f / 3f);
+        SplashParticle.transform.localScale = m_InitScale * scale;
     }
 }
